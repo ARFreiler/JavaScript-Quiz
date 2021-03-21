cdb2afaeb96e813574d2c6cb4e1438fdf173d52a
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-21 19:35:28 -0400

    Created variables pertaining to quiz questions. Adjusted some id's in html to better fit variable naming.

---
+++ main.js
@@ -0,0 +1,22 @@
+// Section
+var quizSection = document.querySelectorAll("quiz-section");
+
+// Start
+var startSection = document.getElementById("start");
+var startButton =document.getElementById("start-button");
+
+// Quiz Questions
+var quizQuestions = document.getElementById("quiz-questions");
+var timeRemaining = document.getElementById("time-remaining");
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+var choiceStatus = document.querySelectorAll(".choice-status");
+var correct = document.getElementById("correct");
+var wrong = document.getElementById("wrong");
+
+var end =
+var endTitle =
+var scoreText =
+var submitScore =
+var initials =
+var error = 
