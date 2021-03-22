9003d93b82dda7ec016ffeb4e4a657f236ad004a
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-22 12:46:23 -0400

    Created function to hide the start section and show the quiz section.

--- main.js
+++ main.js
@@ -23,38 +23,54 @@ var submitScore = document.getElementById("submit-score");
 var initials = document.getElementById("initials");
 var error = document.getElementById("error-message");
 
-// Questions Element
-class Question {
-    constructor(question, choices, indexOfCorrectChoice) {
-      this.question = question;
-      this.choices = choices;
-      this.indexOfCorrectChoice = indexOfCorrectChoice;
+// // Questions Element
+// class Question {
+//     constructor(question, choices, indexOfCorrectChoice) {
+//       this.question = question;
+//       this.choices = choices;
+//       this.indexOfCorrectChoice = indexOfCorrectChoice;
+//     }
+//   }
+
+// var questionOne = new Question("Commonly used data types DO NOT include: ",
+// ["Strings","Booleans", "Alerts", "Numbers"], 2);
+// var questionTwo = new Question("The condition in an if / else statement is enclosed within _____.",
+// ["Quotes", "Braces", "Parantheses", "Square Brackets"], 2);
+// var questionThree = new Question("Arrays in JavaScript can be used to store ____.",
+// ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"], 3);
+// var questionFour = new Question("String values must be enclosed within _____ when being assigned to variables.",
+// ["Commas", "Braces", "Quotes", "Parantheses"], 2);
+// var questionFive = new Question("A very useful tool used during development and debugging for printing content to the debugger is: ",
+// ["JavaScript", "Terminal/Bash", "For Loops", "console.log"], 3);
+// var questionList = [questionOne, questionTwo, questionThree, questionFour, questionFive];
+
+// var currentQuestion = 0;
+// var totalTime = 60;
+// var totalTimeInterval;
+// var choiceStatusTimeout;
+
+document.getElementById("start-button").addEventListener("click", startQuiz);
+
+// This worked to hide element
+// function startQuiz() {
+//   var showQuiz = document.getElementById("start");
+//   if (showQuiz.style.display === "block") {
+//     showQuiz.style.display = "none";
+//   }
+//   }
+
+function startQuiz() {
+  if (document.getElementById("start")) {
+    if (document.getElementById("start").style.display == "block") {
+      document.getElementById("start").style.display = "none";
+      document.getElementById("quiz-questions").style.display = "block";
     }
   }
-
-var questionOne = new Question("Commonly used data types DO NOT include: ",
-["Strings","Booleans", "Alerts", "Numbers"], 2);
-var questionTwo = new Question("The condition in an if / else statement is enclosed within _____.",
-["Quotes", "Braces", "Parantheses", "Square Brackets"], 2);
-var questionThree = new Question("Arrays in JavaScript can be used to store ____.",
-["Numbers and Strings", "Other arrays", "Booleans", "All of the above"], 3);
-var questionFour = new Question("String values must be enclosed within _____ when being assigned to variables.",
-["Commas", "Braces", "Quotes", "Parantheses"], 2);
-var questionFive = new Question("A very useful tool used during development and debugging for printing content to the debugger is: ",
-["JavaScript", "Terminal/Bash", "For Loops", "console.log"], 3);
-var questionList = [questionOne, questionTwo, questionThree, questionFour, questionFive];
-
-var currentQuestion = 0;
-var totalTime = 60;
-var totalTimeInterval;
-var choiceStatusTimeout;
-
-startButton.addEventListener("click", startGame);
-
-function startGame() {
-  showElement(quizQuestions, quizSection);
-
-  displayTime();
-  displayQuestion();
-  startTimer();
 }
+
+
+
+
+// var showQuestions = document.getElementById("quiz-questions");
+  // if (showQuestions.style.display === "none") {
+  //   showQuestions.style.display = "block";}
